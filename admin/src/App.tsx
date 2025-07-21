b919cc0dfa7ea097e0c47d7e11c61a9bc57b449e
Oleksandr Prokopenko <alex.pro.sandok@gmail.com>
2025-07-21 22:31:05 +0300

    created basic reusable app header, using it in admin app

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,9 +3,13 @@ import { HomePage, DashboardPage } from '@admin/src/pages'
 import { AppHeader } from '@ui/components'
 
 export const App = () => {
+  const logo = () => <div>Logo</div>
+  const navigation = () => <nav>Navigation</nav>
+  const userMenu = () => <div>User Menu</div>
+
   return (
     <main>
-      <AppHeader />
+      <AppHeader logo={logo} navigation={navigation} userMenu={userMenu} />
       <Routes>
         <Route index element={<HomePage />} />
 
--- ui/src/components/AppHeader/index.tsx
+++ ui/src/components/AppHeader/index.tsx
@@ -1,3 +1,24 @@
-export const AppHeader = () => {
-  return <div>app header</div>
+import classnames from 'classnames/bind'
+
+import styles from './style.module.scss'
+import { FC, ReactNode } from 'react'
+
+const CN = 'app-header'
+
+const cx = classnames.bind(styles)
+
+interface IHeader {
+  logo: () => ReactNode
+  navigation: () => ReactNode
+  userMenu: () => ReactNode
+}
+
+export const AppHeader: FC<IHeader> = ({ logo, navigation, userMenu }) => {
+  return (
+    <header className={cx(CN)}>
+      {logo()}
+      {navigation()}
+      {userMenu()}
+    </header>
+  )
 }
